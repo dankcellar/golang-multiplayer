3074d50f0dc01a8c2fb9dff93383c4e24cb6c46e
Eric Downey <edowney29@gmail.com>
2020-04-16 01:16:25 -0400

    Update main.go

--- main.go
+++ main.go
@@ -9,24 +9,24 @@ import (
 
 // var addr = flag.String("addr", "127.0.0.1:8080", "http service address")
 
-func servePublic(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	http.FileServer(http.Dir("./public"))
-}
+// func servePublic(w http.ResponseWriter, r *http.Request) {
+// 	log.Println(r.URL)
+// 	if r.URL.Path != "/" {
+// 		http.Error(w, "Not found", http.StatusNotFound)
+// 		return
+// 	}
+// 	if r.Method != "GET" {
+// 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// 		return
+// 	}
+// 	http.FileServer(http.Dir("./public"))
+// }
 
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", servePublic)
+	http.Handle("/", http.FileServer(http.Dir("/public")))
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
