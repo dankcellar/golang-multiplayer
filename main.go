ead1f12b53d1af412a3296bfcf0fe451c8fda1d9
Eric Downey <edowney29@gmail.com>
2020-05-01 20:30:47 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+// var addr = flag.String("addr", ":8080", "http service address")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
@@ -33,8 +34,13 @@ func main() {
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
