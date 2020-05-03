a6eafbc2b2b85663bdac309c6be0caba05b6f1f3
Eric Downey <edowney29@gmail.com>
2020-05-03 01:10:20 -0400

    Update client.go

--- client.go
+++ client.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"time"
@@ -68,7 +69,7 @@ func (c *Client) readPump() {
 			}
 			break
 		}
-		// message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
 		c.hub.broadcast <- message
 	}
 }
